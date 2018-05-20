9a168e02e552019cb898e1a7f775500a23e094f6
ilamart <39222202+ilamart@users.noreply.github.com>
2018-05-20 11:40:29 +0300

    Initial commit

---
+++ calc_op1_op2_operat/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace calc_op1_op2_operat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите пример с использованием пробелов вида <число1> <число2> <действие>:");
+                string[] stnums = Console.ReadLine().Split(' ');
+                switch (stnums[2])
+                {
+                    case "+": Console.WriteLine(double.Parse(stnums[0]) + double.Parse(stnums[1])); break;
+                    case "-": Console.WriteLine(double.Parse(stnums[0]) - double.Parse(stnums[1])); break;
+                    case "*": Console.WriteLine(double.Parse(stnums[0]) * double.Parse(stnums[1])); break;
+                    case "/": Console.WriteLine(double.Parse(stnums[0]) / double.Parse(stnums[1])); break;
+                    default: Console.WriteLine("В качестве действия используйте символы '+', '-', '*', '/'"); break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
